b261a33998b8e1db68a1d5d9389464bd119a49e0
camilo <cristian-camilo.c@hotmail.com>
2019-05-01 22:33:27 -0500

    buscador

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[]= [];
+  constructor(private _heroesService:HeroesService) { }
+
+  ngOnInit() {
+      this.heroes = this._heroesService.getHeroes();
+  }
+
+}
